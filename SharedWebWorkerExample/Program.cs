de33f53ac74bffbd8c735fdf3b56472351c0b647
Todd Tanner <todd@spawndev.com>
2026-01-02 10:39:11 -0500

    Added some comments

--- SharedWebWorkerExample/Program.cs
+++ SharedWebWorkerExample/Program.cs
@@ -5,13 +5,18 @@ using SharedWebWorkerExample.Services;
 using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.WebWorkers;
 
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// SpawnDev.BlazorJS setup
 builder.Services.AddBlazorJSRuntime();
+
+// WebWorkerService
 builder.Services.AddWebWorkerService();
 
+// Test service that will be called in the Window context and run in the SharedWebWorker context
+// See Home.razor for example usage
 builder.Services.AddSingleton<ITestService, TestService>();
 
 await builder.Build().BlazorJSRunAsync();
